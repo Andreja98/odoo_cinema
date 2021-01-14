73299bdefcb4363d0cc4223002d71707f797e9a8
Andreja98 <andreja.bicanin98@gmail.com>
2021-01-14 02:36:55 +0100

    Fixing update_sold_seats method to add tickets on number of sold_seats field

--- odoo-14.0/custom/cinema_management/models/Wizard.py
+++ odoo-14.0/custom/cinema_management/models/Wizard.py
@@ -12,6 +12,12 @@ class Wizard(models.TransientModel):
     product_id = fields.Many2one('product.product', string="Product", ondelete='cascade', domain=[('e_ticket', '=', True)])
 
 
-    # Metoda koja azurira broj prodatih karata preko wizard-a
+    # Metoda koja azurira broj prodatih karata preko wizard-a. Dobija se broj prodatih karata za trenutni timetable
+    # i uvecava se za broj karata unetih za prodaju preko wizarda.
     def update_sold_seats(self):
+        # trenutni_id = self._context.get('active_id')
+        # print(trenutni_id)
+        self._cr.execute("""Select sold_seats from cinema_timetable where id=%s"""%(self._context.get('active_id')))
+        for value in self.env.cr.fetchall():
+            self.tickets += value[0]
         self.env['cinema.timetable'].browse(self._context.get('active_ids')).update({'sold_seats': self.tickets})
